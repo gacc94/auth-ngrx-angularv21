c1ba8aa4e8c3de6def150c48b293f56477516aa6
Gustavo Caqui <gustavocaqui94@gmail.com>
2025-12-15 11:55:17 -0500

    feat: add ngrx-signals

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, MatButtonModule, MatDividerModule, MatIconModule],
     templateUrl: './app.html',
     styleUrl: './app.scss',
 })
